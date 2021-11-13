6a535dd35cb549ccb744fd603782ecc601ab2839
arsyntax <angeline.rodriguez@usm.cl>
2021-11-13 10:51:24 -0300

    modelo usuario

--- HealthyPillMain/HealthyPillApp/forms.py
+++ HealthyPillMain/HealthyPillApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import models
-
+from HealthyPillApp.models import UserProfile
 
 class UserRegisterForm(UserCreationForm):
 
@@ -12,6 +12,6 @@ class UserRegisterForm(UserCreationForm):
 
 
 	class Meta:
-		model = User
+		model = UserProfile
 		fields = ['username', 'email', 'password1', 'password2',]
 		help_texts = {k:'' for k in fields }
--- HealthyPillMain/HealthyPillApp/models.py
+++ HealthyPillMain/HealthyPillApp/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    ''''Manager para perfiles de usuarios'''
+
+    def create_user(self, username, email, password=None):
+        ''''Crear nuevo userprofile'''
+        user = self.create_user(username, email, password)
+        if not email:
+            raise ValueError('Debe ingresar un correo')
+        
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password, email=None):
+        user = self.create_user(username, password, email)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    ''''Modelo Base de datos para usuarios en el sistema'''
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    #name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_pacient = models.BooleanField(default=False)
+    is_specialist = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    #REQUIRED_FIELDS = ['name', 'email']
+    
+    def __str__(self):
+        ''''Retorna cadena representando al usuario'''
+        return self.username
+
+    def __str__(self):
+        ''''Retorna cadena representando al usuario'''
+        return self.email
+
+    #def get_full_name(self):
+    #    ''''Retorna nombre completo'''
+    #    return self.name
+    
+    #def get_short_name(self):
+    #    ''''Retorna nombre corto'''
+    #    return self.name
+
+
+
